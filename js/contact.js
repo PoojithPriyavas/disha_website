11436bcddf1887aa2fc8dbcd37ad06ac1317fee2
poojith-priyavas <poojithpriyavas31@gmail.com>
2024-04-08 16:48:27 +0530

    changes

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,18 @@
 function sendMailJs() {
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+
+    // Check if name or email is empty
+    if (name.trim() === '' || email.trim() === '') {
+        alert("Name and email are required fields");
+        return; // Exit the function early
+    }
+
     var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        messege: document.getElementById("messege").value,
+        name: name,
+        email: email,
+        message: document.getElementById("message").value,
         phone: document.getElementById("phone").value
 
     };
@@ -10,11 +20,7 @@ function sendMailJs() {
     const serviceID = "service_7c40sqm";
     const templateID = "template_dy9gs4u";
 
-    if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
-        // Display an error message or prevent form submission
-        alert('Please fill in all the required fields.');
-        return false; // Prevent form submission
-    }
+
 
 
     emailjs.send(serviceID, templateID, params)
@@ -22,11 +28,11 @@ function sendMailJs() {
             res => {
                 document.getElementById("name").value = "";
                 document.getElementById("email").value = "";
-                document.getElementById("messege").value = "";
+                document.getElementById("message").value = "";
                 document.getElementById("phone").value = "";
 
                 console.log(res);
-                alert("your messege sent succesfully");
+                alert("your message sent succesfully");
 
             }
         ).catch((err) => console.log(err));
